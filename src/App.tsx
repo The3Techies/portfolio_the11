f04457db702f2580b251af1d6e46392fe7af66b7
AboGhanem99 <awsan11111@gmail.com>
2023-05-06 23:26:36 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import MySkills from "./components/MySkills";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
